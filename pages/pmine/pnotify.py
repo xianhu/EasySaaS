d9555f3255ce535ff69388e0e15589d30b54d0b8
qixianhu <qixianhu@qq.com>
2021-11-03 11:34:31 +0800

    add notification, add profile, delete --bs-gutter-x:0

--- pages/pmine/pnotify.py
+++ pages/pmine/pnotify.py
@@ -5,10 +5,19 @@ page of notify
 """
 
 import dash_bootstrap_components as dbc
+from dash import html
 
 
 def layout(pathname, search):
     """
     layout of page
     """
-    return dbc.Container("notify")
+    return html.Div(children=[
+        dbc.Alert("This is a primary alert", color="primary", class_name="w-75"),
+        dbc.Alert("This is a secondary alert", color="secondary", class_name="w-75"),
+        dbc.Alert("This is a success alert! Well done!", color="success", class_name="w-75"),
+        dbc.Alert("This is a warning alert... be careful...", color="warning", class_name="w-75"),
+        dbc.Alert("This is a danger alert. Scary!", color="danger", class_name="w-75"),
+        dbc.Alert("This is an info alert. Good to know!", color="info", class_name="w-75"),
+        dbc.Alert("This is a dark alert", color="dark", class_name="w-75"),
+    ], className="d-flex flex-column align-items-center w-100 py-4 h-100 overflow-scroll")
--- pages/pmine/pprofile.py
+++ pages/pmine/pprofile.py
@@ -4,6 +4,7 @@
 page of profile
 """
 
+from dash import html
 import dash_bootstrap_components as dbc
 
 
@@ -11,4 +12,31 @@ def layout(pathname, search):
     """
     layout of page
     """
-    return dbc.Container("profile")
+    form = dbc.Form(children=[
+        dbc.Row(children=[
+            dbc.Label("username:", html_for="example-email-row", width=4),
+            dbc.Col(dbc.Input(type="text", id="example-email-row",), width=8),
+        ], className="mb-3"),
+        dbc.Row(children=[
+            dbc.Label("phone:", html_for="example-email-row", width=4),
+            dbc.Col(dbc.Input(type="tel", id="",), width=8),
+        ], className="mb-3"),
+        dbc.Button("Save", class_name="w-50")
+    ], class_name="p-4 border rounded bg-light")
+    form1 = dbc.Form(children=[
+        dbc.Row(children=[
+            dbc.Label("Password:", html_for="example-email-row", width=4),
+            dbc.Col(dbc.Input(type="password", id="example-email-row",), width=8),
+        ], className="mb-3"),
+        dbc.Row(children=[
+            dbc.Label("Confirm:", html_for="example-email-row", width=4),
+            dbc.Col(dbc.Input(type="password", id="",), width=8),
+        ], className="mb-3"),
+        dbc.Button("Update", class_name="w-50")
+    ], class_name="p-4 border rounded bg-light")
+    return html.Div(children=[
+        dbc.Row(children=[
+            dbc.Col(form, width=12, md=4, class_name="mt-2"),
+            dbc.Col(form1, width=12, md=4, class_name="mt-2"),
+        ], justify="", class_name="")
+    ], className="w-100 p-4 h-100 overflow-scroll")
