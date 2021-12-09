192b998d4bdd38509488d9c59a603adffbfceaa3
qixianhu <qixianhu@qq.com>
2021-12-09 19:15:11 +0800

    h-100-scroll style

--- pages/panalysis/__init__.py
+++ pages/panalysis/__init__.py
@@ -22,7 +22,7 @@ def layout(pathname, search):
     """
     # define components
     toggler_icon = html.A(html.I(className="bi bi-list fs-1"))
-    white_gap = html.Div(style={"height": "4px"}, className="bg-white")
+    white_gap = html.Div(style={"height": "4px"}, className="bg-light")
 
     # define components
     cat_title, cat_list, cat_active_id = "Upload Data", [], None
@@ -62,16 +62,16 @@ def layout(pathname, search):
         dbc.Col(children=[
             dbc.Collapse([upload_div, white_gap, cat_accordion], id=f"id-{TAG}-collapse", class_name="d-md-block"),
             html.Div("All rights reserved.", className="small-hidden text-muted text-center mt-auto py-2"),
-        ], width=12, md=2, class_name="d-flex flex-column accordion-bg h-100-scroll p-0"),
+        ], width=12, md=2, class_name="d-flex flex-column accordion-bg h-100-scroll-md p-0"),
         dbc.Col(children=[
             html.Div(cat_title, className="text-muted my-2"),
-            html.Div(cat_title, className="bg-white border", style={"min-height": "80%"})
-        ], width=12, md=10, class_name="bg-light h-100 overflow-scroll px-4"),
-    ], align="start", justify="center", class_name="h-100 overflow-scroll w-100 mx-auto")
+            html.Div(cat_title, className="bg-white border rounded", style={"min-height": "900px"})
+        ], width=12, md=10, class_name="bg-light h-100-scroll px-md-4"),
+    ], align="start", justify="center", class_name="h-100-scroll w-100 mx-auto")
 
     # define components
     navbar = cnavbar.layout(pathname, search, fluid=True)
-    content = dbc.Container([small_display, content], fluid=True, class_name="h-100 overflow-scroll p-0")
+    content = dbc.Container([small_display, content], fluid=True, class_name="h-100-scroll p-0")
 
     # return result
     return html.Div(children=[navbar, content], className="d-flex flex-column vh-100 overflow-scroll")
