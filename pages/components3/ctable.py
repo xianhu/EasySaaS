fe96d1da3e47d47888d15a2f751ac7512301737d
qixianhu <qixianhu@qq.com>
2022-03-18 15:52:22 +0800

    table

--- pages/components3/ctable.py
+++ pages/components3/ctable.py
@@ -9,43 +9,43 @@ from dash import html
 
 
 def layout(
-        pathname, search, tid, data, hd_names, row_keys, title_key=None, out_key=None,
-        td_class="text-center p-1", th_class="text-center p-1 font-weight-bolder",
+        pathname, search, tid, data, header_list, key_list,
+        title_key=None, out_key=None, th_class=None, td_class=None, data_class=None,
+        bordered=True, borderless=True, hover=True, striped=True, class_name=None,
 ):
     """
     layout of component
     """
-    # 创建表头，ID为tid-hd，带有两个数据属性：data-now和data-pre
-    hd_list = [html.Th(name, className=th_class) for name in hd_names]
-    thead = html.Thead(html.Tr(hd_list), id=tid + "-hd", **{"data-now": "-1", "data-pre": "-1"})
+    # define class
+    th_class = th_class or "text-center p-1"
+    td_class = td_class or "text-center p-1"
 
-    # 创建行列表
+    # define thead
+    th_list = []
+    if header_list:
+        th_list = [html.Th(name, className=th_class) for name in header_list]
+    thead = html.Thead(html.Tr(th_list), id=tid + "-head")
+
+    # define tr
     tr_list = []
     for index, item_info in enumerate(data or []):
-        # 构建行ID，当selectable为True时，注意不同的ID形式
-        tr_id = {"type": tid + "-row", "index": index}
+        # define title and out's data
+        tr_title = str(item_info.get(title_key, ""))
+        tr_index = item_info.get(out_key, index)
 
-        # 构建行的标题
-        tr_title = item_info.get(title_key, None)
+        # define class list
+        class_info = data_class[index] if data_class else {}
+        class_list = [class_info.get(key, "") for key in key_list]
+        class_list = [" ".join([td_class, _class]) for _class in class_list]
 
-        # 构建行传递的数据，默认为index行号
-        tr_odata = {"data-out": item_info.get(out_key, index)}
+        # define value list and td list
+        value_list = [item_info[key] for key in key_list]
+        td_list = [html.Td(v, className=c) for (v, c) in zip(value_list, class_list)]
 
-        # 构建单元格的值和样式，值和样式之间用$$进行分隔
-        td_data_temp = [str(item_info[key]).split("$$") for key in row_keys]
-        td_value_list = [data[0] if len(data) >= 1 else "" for data in td_data_temp]
+        # define tr_list
+        tr_id = {"type": tid + "-row", "index": tr_index}
+        tr_list.append(html.Tr(td_list, id=tr_id, title=tr_title))
 
-        # 构建单元格的样式，值和样式之间用$$进行分隔
-        td_class_list = [data[1] if len(data) >= 2 else "" for data in td_data_temp]
-        td_class_list = [" ".join([td_class, _class]) for _class in td_class_list]
-
-        # 创建单元格列表及tr组件
-        td_list = [html.Td(td_value_list[i], className=td_class_list[i]) for i in range(len(row_keys))]
-        tr_list.append(html.Tr(td_list, id=tr_id, title=tr_title, **tr_odata, className=""))
-
-    # 考虑数据为空
-    if not tr_list:
-        tr_list.append(html.Tr(html.Td("暂无数据", colSpan=len(row_keys), className=td_class)))
-
-    # 返回创建的表格，包含thead和tbody
-    return dbc.Table(children=[thead, html.Tbody(tr_list)], id=tid, bordered=True, hover=True, striped=True)
+    # return result
+    args = {"bordered": bordered, "borderless": borderless, "hover": hover, "striped": striped}
+    return dbc.Table(children=[thead, html.Tbody(tr_list)], id=tid, **args, class_name=class_name)
--- pages/panalysis/ptable.py
+++ pages/panalysis/ptable.py
@@ -5,9 +5,13 @@ table page
 """
 
 import dash_bootstrap_components as dbc
+import plotly.express as px
 from dash import html
 
+from ..components3 import ctable
+
 TAG = "analysis-table"
+DATA = px.data.iris()
 
 
 def layout(pathname, search):
@@ -25,7 +29,19 @@ def layout(pathname, search):
     ], align="center", justify="between", class_name=None)
 
     # return result
+    class_dict = {"sepal_length": "bg-light"}
     return dbc.Card(children=[
         dbc.CardHeader(row_header, class_name="px-4 py-3"),
-        html.Div(dbc.Table(striped=True, bordered=False, hover=True), className="p-4"),
+        html.Div(children=[
+            ctable.layout(
+                pathname, search,
+                f"id-{TAG}-table1", DATA.to_dict("records")[:10],
+                DATA.columns.to_list(), DATA.columns.to_list(),
+            ),
+            ctable.layout(
+                pathname, search,
+                f"id-{TAG}-table2", DATA.to_dict("records")[20:22], None, DATA.columns.to_list(),
+                data_class=[class_dict, class_dict], striped=False, hover=False,
+            ),
+        ], className="p-4"),
     ], class_name=None, style={"minHeight": "600px"})
