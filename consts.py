168ac153a20bd4139c50d3f374574dc698183dd3
qixianhu <qixianhu@qq.com>
2022-04-02 15:01:42 +0800

    delete utility dir and paths.py (paths -> constants.py

--- consts.py
+++ consts.py
@@ -1,7 +1,7 @@
 # _*_ coding: utf-8 _*_
 
 """
-constants
+Constants File
 """
 
 import re
@@ -10,3 +10,17 @@ import re
 RE_PWD = re.compile(r"^(?![0-9]+$)(?![a-zA-Z]+$)[\d\D]{6,20}$")
 RE_PHONE = re.compile(r"^(13[0-9]|14[5|7]|15[0-9]|18[0-9])\d{8}$")
 RE_EMAIL = re.compile(r"^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$")
+
+# page path
+PATH_ROOT = "/"
+
+PATH_INTROS = "/intros"
+PATH_ANALYSIS = "/analysis"
+
+PATH_LOGIN = "/login"
+PATH_LOGOUT = "/logout"
+
+PATH_REGISTERE = "/registere"
+PATH_RESETPWDE = "/resetpwde"
+
+PATH_USER = "/user"
--- utility/__init__.py
+++
@@ -1,8 +0,0 @@
-# _*_ coding: utf-8 _*_
-
-"""
-utility module
-"""
-
-from .consts import RE_PWD, RE_PHONE, RE_EMAIL
-from .trigger import get_trigger_property
