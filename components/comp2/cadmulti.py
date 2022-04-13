e9caffc861e852b9a3f09c40820a2e933de20222
qixianhu <qixianhu@qq.com>
2022-04-13 14:35:15 +0800

    change path to href in accordion

--- components/comp2/cadmulti.py
+++ components/comp2/cadmulti.py
@@ -16,9 +16,9 @@ def layout(catalog_list, flush=None, class_name=None):
     accordion_item_list = []
     for title_first, id_first, list_second in catalog_list:
         address_list = []
-        for title, _id, path in list_second:
+        for title, _id, href in list_second:
             ctitle = html.Div(title, id=_id, className="text-black hover-primary")
-            address = html.A(ctitle, href=path, className="text-decoration-none px-5 py-2")
+            address = html.A(ctitle, href=href, className="text-decoration-none px-5 py-2")
             address_list.append(address)
 
         # define components
