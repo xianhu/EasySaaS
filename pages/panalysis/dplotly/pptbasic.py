V1.3.3 (#32)  fix spinner, add dash.noupdate, fix bugs in dcc.location(hash), update sign/user/analysis pages
* fix spinner

* fix analysis: remove login

* fix bugs in dcc.location

* update model.py

* add dash.noupdate to sign page

* fix user page (add dash.noupdate, fix bugs in children)

* fix analysis page

pages/panalysis/dplotly/pptbasic.py
@@ -1,7 +1,7 @@
 # _*_ coding: utf-8 _*_
 
 """
-template page
+basic plotly page
 """
 
 import dash_bootstrap_components as dbc
@@ -11,7 +11,7 @@ from dash import Input, Output, dcc, html
 
 from app import app
 
-TAG = "analysis-plotly-template"
+TAG = "analysis-plotly-basic"
 
 
 def layout(pathname, search, **kwargs):
@@ -19,17 +19,17 @@ def layout(pathname, search, **kwargs):
     layout of page
     """
     return dbc.Card(children=[
-        dbc.CardHeader("Charts Template", class_name="px-4 py-3"),
-        html.Div(dbc.Spinner(id=f"id-{TAG}-content"), className="p-4"),
+        dbc.CardHeader("Basic Charts", class_name="px-4 py-3"),
+        dbc.Spinner(html.Div(id=f"id-{TAG}-content", className="p-4")),
     ], class_name=None, style={"minHeight": "600px"})
 
 
 @app.callback(
     Output(f"id-{TAG}-content", "children"),
-    Input(f"id-{TAG}-content", "type"),
+    Input(f"id-{TAG}-content", "className"),
     prevent_initial_call=False,
 )
-def _init_page(spinner_type):
+def _init_page(n_clicks):
     # define variables
     random_x = np.linspace(0, 1, 100)
     random_y0 = np.random.randn(100) + 5