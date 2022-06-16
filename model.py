78d108b5048075950bf877b08a6afcdbd4592ff0
qixianhu <qixianhu@qq.com>
2022-06-16 17:42:04 +0800

    update model, add organizations

--- model.py
+++ model.py
@@ -4,10 +4,11 @@
 Model Defination
 """
 
+import datetime
 import hashlib
 
 import sqlalchemy
-from sqlalchemy import func, orm
+from sqlalchemy import ForeignKey, func, orm
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug import security
 
@@ -17,7 +18,16 @@ from config import config_database_uri
 app_db = SQLAlchemy(app=None)
 
 
-class User(app_db.Model):
+class BaseModel:
+    """
+    base model
+    """
+    status = sqlalchemy.Column(sqlalchemy.Integer, default=1, doc="0 or 1")
+    datetime_create = sqlalchemy.Column(sqlalchemy.DateTime, server_default=func.now())
+    datetime_update = sqlalchemy.Column(sqlalchemy.DateTime, server_default=func.now(), onupdate=func.now())
+
+
+class User(app_db.Model, BaseModel):
     __tablename__ = "users"
 
     id = sqlalchemy.Column(sqlalchemy.String(50), primary_key=True)
@@ -31,46 +41,36 @@ class User(app_db.Model):
     email = sqlalchemy.Column(sqlalchemy.String(50), nullable=True)
     phone = sqlalchemy.Column(sqlalchemy.String(50), nullable=True)
 
-    plan_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("plans.id"))
-    plan_expire = sqlalchemy.Column(sqlalchemy.Integer, default=-1, doc="day count")
-
-    session = sqlalchemy.Column(sqlalchemy.String(500), doc="session value")
-    status = sqlalchemy.Column(sqlalchemy.Integer, default=1, doc="0 or 1")
-
     filename = sqlalchemy.Column(sqlalchemy.String(500), doc="file name")
+    session = sqlalchemy.Column(sqlalchemy.String(500), doc="session value")
     tempcol = sqlalchemy.Column(sqlalchemy.String(500), doc="temporary column")
 
-    datetime_create = sqlalchemy.Column(sqlalchemy.DateTime, server_default=func.now())
-    datetime_update = sqlalchemy.Column(sqlalchemy.DateTime, server_default=func.now(), onupdate=func.now())
-
     # foreign key and relationship
-    plan = orm.relationship("Plan", backref=orm.backref("users"))
+    organ_id = sqlalchemy.Column(sqlalchemy.Integer, ForeignKey("organizations.id"))
+    organization = orm.relationship("Organization", backref=orm.backref("users"))
 
     # print format
     def __repr__(self) -> str:
-        col_list = [self.id, self.name, self.admin, self.email, self.phone]
+        col_list = [self.id, self.name, self.admin, self.email, self.phone, self.organ_id, self.status]
         return f"User <{' - '.join(map(str, col_list))}>"
 
 
-class Plan(app_db.Model):
-    __tablename__ = "plans"
+class Organization(app_db.Model, BaseModel):
+    __tablename__ = "organizations"
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String(50), nullable=False)
+    name = sqlalchemy.Column(sqlalchemy.String(50), nullable=True)
 
-    content = sqlalchemy.Column(sqlalchemy.Text, doc="plan content")
-    price = sqlalchemy.Column(sqlalchemy.Integer, default=0, doc="plan price")
+    avatar = sqlalchemy.Column(sqlalchemy.String(500), nullable=True)
+    address = sqlalchemy.Column(sqlalchemy.String(500), nullable=True)
 
-    pindex = sqlalchemy.Column(sqlalchemy.Integer, default=0, doc="plan index")
-    status = sqlalchemy.Column(sqlalchemy.Integer, default=1, doc="0 or 1")
-
-    datetime_create = sqlalchemy.Column(sqlalchemy.DateTime, server_default=func.now())
-    datetime_update = sqlalchemy.Column(sqlalchemy.DateTime, server_default=func.now(), onupdate=func.now())
+    plan_title = sqlalchemy.Column(sqlalchemy.String(50), nullable=True)
+    plan_expire = sqlalchemy.Column(sqlalchemy.DateTime, doc="expire datetime")
 
     # print format
     def __repr__(self) -> str:
-        col_list = [self.id, self.name, self.price, self.pindex, self.status]
-        return f"User <{' - '.join(map(str, col_list))}>"
+        col_list = [self.id, self.name, self.plan_title, self.plan_expire, self.status]
+        return f"Organizations <{' - '.join(map(str, col_list))}>"
 
 
 def init_db():
@@ -85,7 +85,7 @@ def init_db():
     app_db.Model.metadata.create_all(engine)
 
 
-def add_user(email="aaaa@qq.com", admin=0, plan_id=None):
+def add_user(email="aaaa@qq.com", admin=0):
     """
     add a user to database
     """
@@ -94,13 +94,18 @@ def add_user(email="aaaa@qq.com", admin=0, plan_id=None):
 
     # basic opration with session
     with orm.sessionmaker(engine)() as session:
+        # organization operations
+        plan_expire = datetime.datetime.now() + datetime.timedelta(days=365)
+        organization = Organization(name="Test", plan_title="Free", plan_expire=plan_expire)
+
+        session.add(organization)
+        session.commit()
+        print(session.query(Organization).all())
+
+        # user operations
         _id = hashlib.md5(email.encode()).hexdigest()
         pwd = security.generate_password_hash(email)
-
-        user = User(
-            id=_id, pwd=pwd, admin=admin,
-            email=email, plan_id=plan_id,
-        )
+        user = User(id=_id, pwd=pwd, admin=admin, email=email, organ_id=organization.id)
 
         session.add(user)
         session.commit()
