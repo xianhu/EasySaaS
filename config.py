f6c77aa69171e893faa52e19cc36e1e5e25f4375
qixianhu <qixianhu@qq.com>
2022-09-18 02:18:36 +0800

    add config_text and update model

--- config.py
+++ config.py
@@ -6,20 +6,38 @@ Config File
 
 import os
 
+# define text config
+_config_text_english = {
+    "app_name": "EasySaaS",
+    "login": "Login",
+    "logout": "Logout",
+    "register": "Register",
+    "forget_password": "Forget Password",
+}
+
+_config_text_chinese = {
+    "app_name": "EasySaaS",
+    "login": "登录",
+    "logout": "登出",
+    "register": "注册",
+    "forget_password": "忘记密码",
+}
+
+config_text = _config_text_english
+
 # define prefix
 ENV_PREFIX = "ES"
 
 # define saas application
-config_app_name = "EasySaaS"
 config_app_domain = os.environ.get(f"{ENV_PREFIX}_APP_DOMAIN")
-config_app_secret_key = os.environ.get(f"{ENV_PREFIX}_APP_SECRET_KEY")
+config_secret_key = os.environ.get(f"{ENV_PREFIX}_SECRET_KEY")
 
 # define mail config
 config_mail_server = os.environ.get(f"{ENV_PREFIX}_MAIL_SERVER")
 config_mail_port = os.environ.get(f"{ENV_PREFIX}_MAIL_PORT")
 config_mail_username = os.environ.get(f"{ENV_PREFIX}_MAIL_USERNAME")
 config_mail_password = os.environ.get(f"{ENV_PREFIX}_MAIL_PASSWORD")
-config_mail_sender = (config_app_name, config_mail_username)
+config_mail_sender = (config_text["app_name"], config_mail_username)
 
 # define uri of database
 config_redis_uri = os.environ.get(f"{ENV_PREFIX}_REDIS_URI")
