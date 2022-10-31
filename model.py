21599d5eb3d06077a6f4a10cc09230796d0397c9
qixianhu <qixianhu@qq.com>
2022-10-31 19:51:42 +0800

    update model.py

--- model.py
+++ model.py
@@ -25,33 +25,33 @@ class User(app_db.Model):
     )
 
     # basic
-    id = sqlalchemy.Column(sqlalchemy.String(50), primary_key=True)
-    pwd = sqlalchemy.Column(sqlalchemy.String(500), nullable=False)
+    id = sqlalchemy.Column(sqlalchemy.String(128), primary_key=True)
+    pwd = sqlalchemy.Column(sqlalchemy.String(512), nullable=False)
 
     # informations
-    name = sqlalchemy.Column(sqlalchemy.String(50), nullable=True)
-    avatar = sqlalchemy.Column(sqlalchemy.String(50), nullable=True)
-    email = sqlalchemy.Column(sqlalchemy.String(50), nullable=True)
-    phone = sqlalchemy.Column(sqlalchemy.String(50), nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String(128), nullable=True)
+    avatar = sqlalchemy.Column(sqlalchemy.String(128), nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String(128), nullable=True)
+    phone = sqlalchemy.Column(sqlalchemy.String(128), nullable=True)
 
     # company
-    company_name = sqlalchemy.Column(sqlalchemy.String(50), nullable=True)
-    company_avatar = sqlalchemy.Column(sqlalchemy.String(50), nullable=True)
+    company_name = sqlalchemy.Column(sqlalchemy.String(128), nullable=True)
+    company_avatar = sqlalchemy.Column(sqlalchemy.String(128), nullable=True)
 
     # address
-    addr_state = sqlalchemy.Column(sqlalchemy.String(50), doc="state of address")
-    addr_city = sqlalchemy.Column(sqlalchemy.String(50), doc="city of address")
-    addr_detail = sqlalchemy.Column(sqlalchemy.String(50), doc="detail of address")
+    addr_state = sqlalchemy.Column(sqlalchemy.String(128), doc="state of address")
+    addr_city = sqlalchemy.Column(sqlalchemy.String(128), doc="city of address")
+    addr_detail = sqlalchemy.Column(sqlalchemy.String(128), doc="detail of address")
 
     # others
-    filename = sqlalchemy.Column(sqlalchemy.String(500), doc="file name")
-    session = sqlalchemy.Column(sqlalchemy.String(500), doc="session value")
-    tempcol = sqlalchemy.Column(sqlalchemy.String(500), doc="temporary column")
+    filename = sqlalchemy.Column(sqlalchemy.String(255), doc="file name")
+    session = sqlalchemy.Column(sqlalchemy.String(255), doc="session value")
+    tempcol = sqlalchemy.Column(sqlalchemy.String(255), doc="temporary column")
 
     # normal columns
     status = sqlalchemy.Column(sqlalchemy.Integer, default=1, doc="-1 0 1")
     datetime_create = sqlalchemy.Column(sqlalchemy.DateTime, server_default=func.now())
-    datetime_update = sqlalchemy.Column(sqlalchemy.DateTime, server_default=func.now(), onupdate=func.now())
+    datetime_update = sqlalchemy.Column(sqlalchemy.DateTime, server_default=func.now(), server_onupdate=func.now())
 
     # print format
     def __repr__(self) -> str:
