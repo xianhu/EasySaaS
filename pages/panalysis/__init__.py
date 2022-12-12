3ccdd436869b76d89c7d2a801dbc78b93ff90f89
qixianhu <qixianhu@qq.com>
2022-12-12 21:26:50 +0800

    analysis page: add switch and store_data

--- pages/panalysis/__init__.py
+++ pages/panalysis/__init__.py
@@ -8,10 +8,8 @@ import dash
 import feffery_antd_components as fac
 import feffery_utils_components as fuc
 import flask_login
-from dash import Input, Output, State, html
+from dash import Input, Output, State, dcc, html
 
-from utility.consts import FMT_EXECUTEJS_HREF
-from utility.paths import PATH_LOGIN
 from .routers import ROUTER_MENU
 
 TAG = "analysis"
@@ -24,6 +22,10 @@ def layout(pathname, search, **kwargs):
     # user instance
     user = flask_login.current_user
     user_title = user.email.split("@")[0]
+    store_data = dict(
+        pathname=pathname, search=search,
+        user_id=user.id, user_email=user.email,
+    )
 
     # define components
     sider = fac.AntdSider(children=[
@@ -34,12 +36,20 @@ def layout(pathname, search, **kwargs):
     content = fac.AntdContent(children=[
         fac.AntdRow(children=[
             fac.AntdCol(id=f"id-{TAG}-header", className="fs-6 fw-bold"),
-            fac.AntdCol(fac.AntdBadge(fac.AntdDropdown(menuItems=[
-                {"title": "Profile", "key": "Profile"},
-                {"title": "Settings", "key": "Settings"},
-                {"isDivider": True},
-                {"title": "Logout", "href": "/login"},
-            ], id=f"id-{TAG}-user", title=user_title, buttonMode=True), dot=True)),
+            fac.AntdCol(children=[
+                fac.AntdSwitch(
+                    id=f"id-{TAG}-switch",
+                    checkedChildren="Open",
+                    unCheckedChildren="Close",
+                    className="me-2",
+                ),
+                fac.AntdBadge(fac.AntdDropdown(menuItems=[
+                    {"title": "Profile", "key": "Profile"},
+                    {"title": "Settings", "key": "Settings"},
+                    {"isDivider": True},
+                    {"title": "Logout", "href": "/login"},
+                ], id=f"id-{TAG}-user", title=user_title, buttonMode=True), dot=True),
+            ], className=None),
         ], align="middle", justify="space-between", className="bg-white sticky-top px-4 py-2"),
         fuc.FefferyTopProgress(children=[
             html.Div(id=f"id-{TAG}-content", className="px-4 py-4"),
@@ -52,6 +62,7 @@ def layout(pathname, search, **kwargs):
         # define components
         fuc.FefferyExecuteJs(id=f"id-{TAG}-executejs"),
         fuc.FefferyWindowSize(id=f"id-{TAG}-windowsize"),
+        dcc.Store(id=f"id-{TAG}-data", data=store_data),
     ], className="vh-100 overflow-auto")
 
 
@@ -65,34 +76,30 @@ def layout(pathname, search, **kwargs):
 ], [
     Input(f"id-{TAG}-menu", "currentKey"),
     Input(f"id-{TAG}-user", "clickedKey"),
+    Input(f"id-{TAG}-switch", "checked"),
     State(f"id-{TAG}-windowsize", "_width"),
     State(f"id-{TAG}-windowsize", "_height"),
+    State(f"id-{TAG}-data", "data"),
 ], prevent_initial_call=False)
-def _update_content(current_key, clicked_key, width, height):
-    # define outputs
-    out_key = dict(current_key=current_key, clicked_key=None)
-    out_content = dict(header="", content=fac.AntdEmpty(locale="en_US"))
-    out_executejs = dash.no_update
-
-    # check user
-    user = flask_login.current_user
-    if not user.is_authenticated:
-        out_executejs = FMT_EXECUTEJS_HREF.format(PATH_LOGIN)
-        return out_key, out_content, out_executejs
-
-    # check trigger
+def _update_content(current_key, clicked_key, switch_checked, _width, _height, store_data):
+    # check trigger (default)
     trigger_id = dash.ctx.triggered_id
     if not trigger_id:
         trigger_id = f"id-{TAG}-menu"
         current_key = ROUTER_MENU[0]["props"]["key"]
 
     # define components
-    if trigger_id == f"id-{TAG}-menu":
-        out_key["current_key"] = current_key
-        out_content["header"] = f"{current_key}({width}x{height})"
-    else:
-        out_key["current_key"] = None
-        out_content["header"] = f"{clicked_key}({width}x{height})"
+    if (trigger_id == f"id-{TAG}-switch" and current_key) or \
+            trigger_id == f"id-{TAG}-menu":
+        clicked_key = None
+    elif (trigger_id == f"id-{TAG}-switch" and clicked_key) or \
+            trigger_id == f"id-{TAG}-user":
+        current_key = None
+    out_key = dict(current_key=current_key, clicked_key=clicked_key)
+
+    # define components
+    header = f"{current_key}-{clicked_key}-{switch_checked}({_width}x{_height})"
+    out_content = dict(header=header, content=fac.AntdEmpty(locale="en_US"))
 
     # return result
-    return out_key, out_content, out_executejs
+    return out_key, out_content, dash.no_update
