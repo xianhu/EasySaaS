418beb704181f848fa5441987fa90dadedcb6044
qixianhu <qixianhu@qq.com>
2023-01-15 15:29:45 +0800

    update user model, add team model

--- models/__init__.py
+++ models/__init__.py
@@ -4,16 +4,15 @@
 models module
 """
 
+import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
 # create SQLAlchemy
 app_db = SQLAlchemy(app=None)
 
 
 class BaseModel(app_db.Model):
-    """
-    base model
-    """
     __abstract__ = True
 
     # get function
@@ -24,3 +23,8 @@ class BaseModel(app_db.Model):
     def to_dict(self) -> dict:
         columns = self.__table__.columns
         return {c.name: getattr(self, c.name) for c in columns}
+
+    # normal columns
+    status = sqlalchemy.Column(sqlalchemy.Integer, default=1, doc="-1 0 1")
+    datetime_created = sqlalchemy.Column(sqlalchemy.DateTime, server_default=func.now())
+    datetime_updated = sqlalchemy.Column(sqlalchemy.DateTime, server_default=func.now(), server_onupdate=func.now())
--- models/users.py
+++ models/users.py
@@ -8,7 +8,6 @@ import hashlib
 import logging
 
 import sqlalchemy
-from sqlalchemy import func
 from werkzeug import security
 
 from models import BaseModel
@@ -30,29 +29,49 @@ class User(BaseModel):
     name = sqlalchemy.Column(sqlalchemy.String(255), nullable=True)
     email = sqlalchemy.Column(sqlalchemy.String(255), nullable=True)
     phone = sqlalchemy.Column(sqlalchemy.String(255), nullable=True)
-    avatar = sqlalchemy.Column(sqlalchemy.String(255), nullable=True)
+    avatar = sqlalchemy.Column(sqlalchemy.String(512), nullable=True)
+
+    # authentication
+    auth_github = sqlalchemy.Column(sqlalchemy.String(512), doc="token of github")
+    auth_google = sqlalchemy.Column(sqlalchemy.String(512), doc="token of google")
+
+
+class Team(BaseModel):
+    __tablename__ = "teams"
+    __table_args__ = (
+        sqlalchemy.Index("index_t_1", "name"),
+    )
+
+    # basic
+    id = sqlalchemy.Column(sqlalchemy.String(255), primary_key=True)
+
+    # informations
+    name = sqlalchemy.Column(sqlalchemy.String(255), nullable=True)
+    avatar = sqlalchemy.Column(sqlalchemy.String(512), nullable=True)
+    address = sqlalchemy.Column(sqlalchemy.String(512), nullable=True)
 
     # informations
     ts_start = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     ts_expired = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
 
-    # company
-    company_name = sqlalchemy.Column(sqlalchemy.String(255), nullable=True)
-    company_avatar = sqlalchemy.Column(sqlalchemy.String(255), nullable=True)
 
-    # address
-    addr_state = sqlalchemy.Column(sqlalchemy.String(255), doc="state of address")
-    addr_city = sqlalchemy.Column(sqlalchemy.String(255), doc="city of address")
-    addr_detail = sqlalchemy.Column(sqlalchemy.String(1024), doc="detail of address")
+class TeamUser(BaseModel):
+    __tablename__ = "team_users"
+    __table_args__ = (
+        sqlalchemy.Index("index_tu_1", "user_id"),
+        sqlalchemy.Index("index_tu_2", "team_id"),
+    )
 
-    # authentication
-    auth_github = sqlalchemy.Column(sqlalchemy.String(255), doc="token of github")
-    auth_google = sqlalchemy.Column(sqlalchemy.String(255), doc="token of google")
+    # basic
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
 
-    # normal columns
-    status = sqlalchemy.Column(sqlalchemy.Integer, default=1, doc="-1 0 1")
-    datetime_created = sqlalchemy.Column(sqlalchemy.DateTime, server_default=func.now())
-    datetime_updated = sqlalchemy.Column(sqlalchemy.DateTime, server_default=func.now(), server_onupdate=func.now())
+    # relations
+    user_id = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    team_id = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+
+    # informations
+    role = sqlalchemy.Column(sqlalchemy.String(255), default="admin", doc="admin/member")
+    role_json = sqlalchemy.Column(sqlalchemy.JSON, nullable=True, doc="json of role")
 
 
 if __name__ == "__main__":
@@ -67,19 +86,43 @@ if __name__ == "__main__":
     User.__table__.create(engine, checkfirst=True)
     logging.warning("create table success: %s", User.__tablename__)
 
+    Team.__table__.drop(engine, checkfirst=True)
+    Team.__table__.create(engine, checkfirst=True)
+    logging.warning("create table success: %s", Team.__tablename__)
+
+    TeamUser.__table__.drop(engine, checkfirst=True)
+    TeamUser.__table__.create(engine, checkfirst=True)
+    logging.warning("create table success: %s", TeamUser.__tablename__)
+
     # create session and add data
+    _email = "admin@easysaas.com"
     with orm.sessionmaker(engine)() as session:
-        _email = "admin@easysaas.com"
         _id = hashlib.md5(_email.encode()).hexdigest()
         _pwd = security.generate_password_hash(_email)
-        _user = User(id=_id, pwd=_pwd, email=_email)
+        _user = User(id=_id, pwd=_pwd, email=_email, name="admin")
 
-        session.add(_user)
-        session.commit()
-        logging.warning("create user success: %s", _user.get("email"))
+        try:
+            session.add(_user)
+            session.commit()
+            logging.warning("add user success: %s", _user.to_dict())
+        except Exception as excep:
+            logging.error("add user failed: %s", excep)
+            session.rollback()
 
-        _user = session.query(User).filter(
-            User.email == _email
-        ).first()
-        _user_dict = _user.to_dict()
-        logging.warning("get user success: %s", _user_dict)
+    # create session and add data
+    _name = "demo team"
+    with orm.sessionmaker(engine)() as session:
+        _user = session.query(User).filter(User.email == _email).first()
+
+        _id = hashlib.md5(_name.encode()).hexdigest()
+        _team = Team(id=_id, name=_name, ts_start=None, ts_expired=None)
+        _team_user = TeamUser(user_id=_user.get("id"), team_id=_team.get("id"), role="admin")
+
+        try:
+            session.add(_team)
+            session.add(_team_user)
+            session.commit()
+            logging.warning("add team success: %s", _team.to_dict())
+        except Exception as excep:
+            logging.error("add team failed: %s", excep)
+            session.rollback()
