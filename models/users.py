7d15d3499750f2c647b81b4af2318bec15b127c0
qixianhu <qixianhu@qq.com>
2023-01-16 17:16:53 +0800

    fix bugs: user_id-projectname

--- models/users.py
+++ models/users.py
@@ -43,7 +43,8 @@ class User(BaseModel):
         """
         get id by email
         """
-        return hashlib.md5(email.encode()).hexdigest()
+        str_encode = email.encode()
+        return hashlib.md5(str_encode).hexdigest()
 
 
 class Project(BaseModel):
@@ -68,11 +69,12 @@ class Project(BaseModel):
     users = sqlalchemy.orm.relationship("User", secondary="project_users", back_populates="projects")
 
     @staticmethod
-    def get_id_by_name(name):
+    def get_id_by_name(name, user_id):
         """
-        get id by name
+        get id by name and user id
         """
-        return hashlib.md5(name.encode()).hexdigest()
+        str_encode = f"{name}-{user_id}".encode()
+        return hashlib.md5(str_encode).hexdigest()
 
 
 class ProjectUser(BaseModel):
@@ -110,7 +112,7 @@ if __name__ == "__main__":
     with orm.sessionmaker(engine)() as session:
         _id = User.get_id_by_email(_email)
         _pwd = security.generate_password_hash(_email)
-        _user = User(id=_id, pwd=_pwd, name="admin", email=_email)
+        _user = User(id=_id, pwd=_pwd, email=_email, name="admin")
 
         try:
             session.add(_user)
@@ -125,18 +127,17 @@ if __name__ == "__main__":
     with orm.sessionmaker(engine)() as session:
         _user = session.query(User).filter(User.email == _email).first()
 
-        _id = Project.get_id_by_name(_project_name)
-        _project = Project(id=_id, name=_project_name)
+        _id = Project.get_id_by_name(_project_name, _user.id)
+        _project = Project(id=_id, name=_project_name, avatar=None)
         _project_user = ProjectUser(user_id=_user.id, project_id=_project.id, role="admin")
 
         try:
-            session.add(_project)
-            session.add(_project_user)
+            session.add_all([_project, _project_user])
             session.commit()
             logging.warning("add project success: %s", _project.to_dict())
             logging.warning("add project_user success: %s", _project_user.to_dict())
         except Exception as excep:
-            logging.error("add project[_user] failed: %s", excep)
+            logging.error("add project/project_user failed: %s", excep)
             session.rollback()
 
     # create session and select data
