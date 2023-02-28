38e857dd6cb3857aba9fd88558424604c63385e2
qixianhu <qixianhu@qq.com>
2023-02-28 15:54:43 +0800

    update user page, add check of tab's name

--- pages/puser/__init__.py
+++ pages/puser/__init__.py
@@ -4,9 +4,13 @@
 user page
 """
 
+import logging
+import urllib.parse
+
 import flask_login
 from dash import html
 
+from .. import palert
 from ..comps import header as comps_header
 
 TAG = "user"
@@ -20,10 +24,19 @@ def layout(pathname, search, **kwargs):
     current_user = flask_login.current_user
     user_title = current_user.email.split("@")[0]
 
+    # check tab's name
+    try:
+        search = urllib.parse.parse_qs(search.lstrip("?").strip())
+        assert search["tab"][0] in ("profile", "settings")
+    except Exception as excep:
+        logging.error("get tab failed: %s", excep)
+        return palert.layout_500(pathname, search)
+    tab = search["tab"][0]
+
     # return result
     return html.Div(children=[
         comps_header.get_component_header(user_title=user_title, dot=True),
         html.Div(children=[
-            html.Span("coming soon..."),
+            html.Span(f"[{tab}] coming soon..."),
         ], className="w-75 m-auto mt-4"),
     ], className="bg-main vh-100 overflow-auto")
