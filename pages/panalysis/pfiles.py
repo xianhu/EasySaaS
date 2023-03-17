20a50ccc29455581854fbe12e32e6605164d3038
qixianhu <qixianhu@qq.com>
2023-03-17 18:25:13 +0800

    update pfiles

--- pages/panalysis/pfiles.py
+++ pages/panalysis/pfiles.py
@@ -1,15 +1,16 @@
 # _*_ coding: utf-8 _*_
 
 """
-intros page
+files page
 """
 
-import random
+import os
 
-import dash
 import feffery_antd_components as fac
-import feffery_utils_components as fuc
-from dash import Input, Output, ClientsideFunction, dcc, html
+from dash import html
+from flask import request
+
+from app import server
 
 TAG_BASE = "analysis"
 TAG = "analysis-files"
@@ -19,20 +20,26 @@ def layout(pathname, search, **kwargs):
     """
     layout of page
     """
-    # define data
-    chart_data = dict(
-        id_chart_div=f"id-{TAG}-chart-div",  # show chart
-        id_chart_click=f"id-{TAG}-chart-click",  # store click data
-        x_data=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        y_data=[random.randint(50, 100) for _ in range(10)],
-    )
+    # return result
+    return html.Div(fac.AntdUpload(buttonContent="upload", apiUrl="/upload/", multiple=True))
+
+
+# upload file
+@server.route("/upload/", methods=["POST"])
+def _route_upload():
+    _id = request.values.get("uploadId")
+    filename = request.files["file"].filename
+
+    # mkdir if not exists
+    path = f"/tmp/{_id}"
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    # write file to path
+    chunk_size = 1024 * 1024
+    with open(f"{path}/{filename}", "wb") as f:
+        for chunk in iter(lambda: request.files["file"].read(chunk_size), b''):
+            f.write(chunk)
 
     # return result
-    style = {"height": "500px"}
-    return html.Div(children=[
-        html.Div(id=f"id-{TAG}-message"),
-        html.Div(id=f"id-{TAG}-chart-div", style=style),  # show chart
-        fuc.FefferySessionStorage(id=f"id-{TAG}-chart-click"),  # store click data
-        dcc.Store(id=f"id-{TAG}-chart-data", data=chart_data),  # trigger clientside callback
-    ], className=None)
-
+    return {"filename": filename}
