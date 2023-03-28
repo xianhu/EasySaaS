85543fa9e346920c27adfa0adc36d6a8152d3449
qixianhu <qixianhu@qq.com>
2023-03-28 10:06:48 +0800

    delete add_user add_project function

--- models/users.py
+++ models/users.py
@@ -12,7 +12,6 @@ from sqlalchemy import orm
 from werkzeug import security
 
 from models import BaseModel
-from utility import get_md5
 
 
 class User(BaseModel):
@@ -90,52 +89,9 @@ class UserProject(BaseModel):
     project = orm.relationship("Project", back_populates="user_projects")
 
 
-def add_user(session, email, pwd=None, project_id=None, role="admin"):
-    """
-    add user, and add user-project relationship if necessary
-    """
-    # add user if necessary, or update password
-    user = session.query(User).filter(User.email == email).first()
-    if not user:
-        user = User(id=get_md5(email), email=email, status=1)
-        user.set_password_hash(pwd)
-        session.add(user)
-        session.commit()
-    elif pwd:
-        user.set_password_hash(pwd)
-        session.commit()
-
-    # add user-project relationship if necessary
-    if project_id:
-        user_project = UserProject(user_id=user.id, project_id=project_id, role=role)
-        session.add(user_project)
-        session.commit()
-
-    # return
-    return user
-
-
-def add_project(session, name, desc=None, user_id=None, role="admin"):
-    """
-    add project, and add user-project relationship
-    """
-    # add project if necessary
-    project = Project(id=get_md5(name + str(time.time())), name=name, desc=desc)
-    session.add(project)
-    session.commit()
-
-    # add user-project relationship if necessary
-    if user_id:
-        user_project = UserProject(user_id=user_id, project_id=project.id, role=role)
-        session.add(user_project)
-        session.commit()
-
-    # return
-    return project
-
-
 if __name__ == "__main__":
     from config import CONFIG_DATABASE_URI
+    from utility import get_md5
 
     # create engine
     engine = sqlalchemy.create_engine(CONFIG_DATABASE_URI)
@@ -145,31 +101,21 @@ if __name__ == "__main__":
     BaseModel.metadata.create_all(engine, checkfirst=True)
 
     # =============================== test ===============================
-    # create session and add data
-    _email = "admin@easysaas.com"
     with orm.sessionmaker(engine)() as _session:
-        _user = add_user(_session, _email, "a123456")
+        _email = "admin@easysaas.com"
+        _user = User(id=get_md5(_email), email=_email, status=1)
+        _user.set_password_hash("a123456")
+        _session.add(_user)
+        _session.commit()
         logging.warning("add user: %s", _user.to_dict())
-    _user_id = _user.id
 
-    # create session and add data
-    _project_name = "demo project"
-    with orm.sessionmaker(engine)() as _session:
-        _project = add_project(_session, _project_name, user_id=_user_id)
+        _project_name = "demo project"
+        _project = Project(id=get_md5(_project_name + str(time.time())), name=_project_name)
+        _session.add(_project)
+        _session.commit()
         logging.warning("add project: %s", _project.to_dict())
-    _project_id = _project.id
 
-    # =============================== test ===============================
-    # create session and select data
-    with orm.sessionmaker(engine)() as _session:
-        _user = _session.get(User, _user_id)
-        logging.warning("get user: %s", _user.to_dict())
-        for up in _user.user_projects:
-            logging.warning("\tuser-project: %s", up.to_dict())
-            logging.warning("\tproject: %s", up.project.to_dict())
-
-        _project = _session.get(Project, _project_id)
-        logging.warning("get project: %s", _project.to_dict())
-        for up in _project.user_projects:
-            logging.warning("\tuser-project: %s", up.to_dict())
-            logging.warning("\tuser: %s", up.user.to_dict())
+        _user_project = UserProject(user_id=_user.id, project_id=_project.id, role="admin")
+        _session.add(_user_project)
+        _session.commit()
+        logging.warning("add user-project: %s", _user_project.to_dict())
