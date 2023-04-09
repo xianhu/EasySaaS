b0043cc65da62a0def2fdfce9bd14e2b7b6548d9
xianhu <qixianhu@qq.com>
2023-04-09 18:55:32 +0800

    update

--- assets/javascripts/client_flow.js
+++ assets/javascripts/client_flow.js
@@ -14,7 +14,7 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
             // add input to div
             let id_input = 'id-input-file';
             let div = document.getElementById(id_div);
-            div.innerHTML = "<input id='id-input-file' type='file' name='file'/>";
+            div.innerHTML = "<input id='id-input-file' type='file' name='file' />";
 
             // bind button click event to input click
             document.getElementById(id_button).addEventListener('click', function (event) {
@@ -24,7 +24,7 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
             // bind input change event to flow upload
             document.getElementById(id_input).addEventListener('change', function (event) {
                 flow.addFile(event.target.files[0]);
-                flow.upload();
+                flow.upload();  // or triggered by session[id-storage-flow]
             });
         },
     },
