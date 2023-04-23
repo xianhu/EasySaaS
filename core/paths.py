33e2adc0b7bdf4951899422f3000e041cc7ec7c5
xianhu <qixianhu@qq.com>
2023-04-23 19:36:24 +0800

    update

--- core/paths.py
+++ core/paths.py
@@ -6,6 +6,7 @@ page paths
 
 PATH_ROOT = "/"
 
+# path for saas
 PATH_LOGIN = "/login"
 PATH_SIGNUP = "/signup"
 PATH_FORGOTPWD = "/forgotpwd"
@@ -14,4 +15,5 @@ PATH_USER = "/user"
 PATH_PROJECTS = "/projects"
 PATH_ANALYSIS = "/analysis"
 
+# path for api
 PATH_API_V1 = "/api/v1"
