29b792673770d194af8f9a497f7d062a6ae39eb4
xianhu <qixianhu@qq.com>
2023-04-26 11:03:32 +0800

    update crud_xxx

--- models/crud/project.py
+++ models/crud/project.py
@@ -16,9 +16,7 @@ from ..schemas import ProjectCreate, ProjectUpdate
 class CRUDProject(CRUDBase[Project, ProjectCreate, ProjectUpdate]):
 
     def get_by_user_id(self, db: Session, user_id: int) -> List[Project]:
-        obj_db_list = db.query(Project).filter(
-            Project.user_id == user_id,
-        ).all()
+        obj_db_list = db.query(Project).filter(Project.user_id == user_id).all()
         return obj_db_list
 
 
