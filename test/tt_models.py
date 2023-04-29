eae4263dd4f661d1be420959642fa5c766498cb7
xianhu <qixianhu@qq.com>
2023-04-29 10:28:30 +0800

    update tt_models

--- test/tt_models.py
+++ test/tt_models.py
@@ -6,7 +6,7 @@ test models
 
 import logging
 
-from core.utils.security import get_pwd_hash
+from core.utils import security
 from models import DbMaker, engine
 from models.base import Model
 from models.crud import crud_project, crud_user
@@ -18,9 +18,12 @@ Model.metadata.drop_all(engine, checkfirst=True)
 Model.metadata.create_all(engine, checkfirst=True)
 
 with DbMaker() as db:
-    # create user
+    # user info =========================================================================
     email = "admin@easysaas.com"
-    user_schema = UserCreate(pwd=get_pwd_hash("a123456"), email=email, id=1001)
+    pwd_hash = security.get_pwd_hash("a123456")
+
+    # create user
+    user_schema = UserCreate(id=1001, pwd=pwd_hash, email=email, email_verified=False)
     user_db = crud_user.create(db, obj_schema=user_schema)
     logging.warning("create user: %s", user_db.to_dict())
 
@@ -29,9 +32,12 @@ with DbMaker() as db:
     user_db = crud_user.update(db, obj_db=user_db, obj_schema=user_schema)
     logging.warning("update user: %s", user_db.to_dict())
 
-    # create project
+    # project info ======================================================================
+    user_id = user_db.id
     project_name = "demo project"
-    project_schema = ProjectCreate(name=project_name, user_id=user_db.id, id=10001)
+
+    # create project
+    project_schema = ProjectCreate(id=10001, name=project_name, user_id=user_id)
     project_db = crud_project.create(db, obj_schema=project_schema)
     logging.warning("create project: %s", project_db.to_dict())
 
@@ -40,7 +46,7 @@ with DbMaker() as db:
     project_db = crud_project.update(db, obj_db=project_db, obj_schema=project_schema)
     logging.warning("update project: %s", project_db.to_dict())
 
-    # test relationship
+    # test relationship =================================================================
     logging.warning("user -> projects: %s", user_db.projects)
     logging.warning("project -> user: %s", project_db.user)
 
