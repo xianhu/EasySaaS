83ae04009d3740355930c68c9292bedea0e74c44
xianhu <qixianhu@qq.com>
2023-05-02 21:50:01 +0800

    update core

--- core/utils/utemail.py
+++ core/utils/utemail.py
@@ -24,15 +24,12 @@ smtp_options = {
 }
 
 
-def send_email(
-        mail_from: Union[str, tuple], mail_to: Union[str, tuple],
-        subject: str, html: str, render: Dict[str, Any],
-) -> int:
+def send_email(_from: Union[str, tuple], _to: Union[str, tuple], subject: str, html: str, render: Dict[str, Any]) -> int:
     """
     send email via smtp
     """
-    message = emails.Message(mail_from=mail_from, subject=JinjaTemplate(subject), html=JinjaTemplate(html))
-    response = message.send(to=mail_to, render=render, smtp=smtp_options)
+    message = emails.Message(mail_from=_from, subject=JinjaTemplate(subject), html=JinjaTemplate(html))
+    response = message.send(to=_to, render=render, smtp=smtp_options)
     return response.status_code  # 250
 
 
@@ -60,4 +57,6 @@ def send_email_verify(email: str, is_code: bool = True, _type: str = None) -> Op
     # send email and check status
     mail_from = (settings.APP_NAME, settings.MAIL_USERNAME)
     status = send_email(mail_from, email, subject=mail_subject, html=mail_html, render=render)
+
+    # return token or None
     return token if status == 250 else None
