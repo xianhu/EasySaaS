2f6361cf0107e70a518d41efe161cfa4b9732b53
xianhu <qixianhu@qq.com>
2023-05-06 10:09:38 +0800

    update tasks

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -8,16 +8,12 @@ from celery import Celery
 
 from core.settings import settings
 
-# define broker and backend
-broker = f"{settings.REDIS_URI}/11"
-backend = f"{settings.REDIS_URI}/12"
-
 # celery -A tasks worker -l INFO --purge
 # celery -A tasks flower --port=5555 -l INFO
 app_celery = Celery(
     __name__,
-    broker=broker,
-    backend=backend,
+    broker=f"{settings.REDIS_URI}/11",
+    backend=f"{settings.REDIS_URI}/12",
     accept_content=["json", ],
     task_serializer="json",
     result_serializer="json",
