4bdc59b847b0267336af41e7b8fff71fde050f45
xianhu <qixianhu@qq.com>
2023-05-15 22:39:18 +0800

    add upload to file api

--- api/__init__.py
+++ api/__init__.py
@@ -6,12 +6,13 @@ api module
 
 from fastapi import APIRouter
 
-from . import auth, project, user
+from . import auth, files, project, user
 
 # define api_router
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(user.router, prefix="/user", tags=["user"])
+api_router.include_router(files.router, prefix="/files", tags=["files"])
 api_router.include_router(project.router, prefix="/project", tags=["project"])
 
 
