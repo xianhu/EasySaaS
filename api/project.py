c5fb1a5dccb6b5a5b7c67814c0ccccb187f84f31
xianhu <qixianhu@qq.com>
2023-05-17 21:43:10 +0800

    update project api

--- api/project.py
+++ api/project.py
@@ -6,13 +6,14 @@ project api
 
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
+from core.settings import error_tips
 from data import get_session
 from data.crud import crud_project
 from data.models import User
-from data.schemas import ProjectCreate, ProjectSchema
+from data.schemas import ProjectCreate, ProjectCreatePri, ProjectSchema
 from .utils import get_current_user
 
 # define router
@@ -24,6 +25,8 @@ def _create(project_schema: ProjectCreate, current_user: User = Depends(get_curr
     """
     create project, and return schema of project
     """
+    user_id = current_user.id
+    project_schema = ProjectCreatePri(user_id=user_id, **project_schema.dict(exclude_unset=True))
     project_model = crud_project.create(session, obj_schema=project_schema)
     return ProjectSchema(**project_model.to_dict())
 
@@ -33,7 +36,8 @@ def _get(current_user: User = Depends(get_current_user), session: Session = Depe
     """
     get schema of project which is current
     """
-    project_model = crud_project.get_current_of_user(session, user_id=current_user.id)
+    user_id = current_user.id
+    project_model = crud_project.get_current_of_user(session, user_id=user_id)
     return ProjectSchema(**project_model.to_dict())
 
 
@@ -42,5 +46,32 @@ def _get_multi(current_user: User = Depends(get_current_user), session: Session 
     """
     get schema of projects which belong to current_user
     """
-    project_model_list = crud_project.get_multi_of_user(session, user_id=current_user.id)
+    user_id = current_user.id
+    project_model_list = crud_project.get_multi_of_user(session, user_id=user_id)
     return [ProjectSchema(**project_model.to_dict()) for project_model in project_model_list]
+
+
+@router.get("/get/{project_id}", response_model=ProjectSchema)
+def _get_by_id(project_id: int, current_user: User = Depends(get_current_user), session: Session = Depends(get_session)):
+    """
+    get schema of project by project_id
+    """
+    user_id = current_user.id
+    project_model = crud_project.get(session, _id=project_id)
+    if project_model.user_id != user_id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=error_tips.QUERY_FAILED,
+        )
+    return ProjectSchema(**project_model.to_dict())
+
+
+@router.post("/update", response_model=ProjectSchema)
+def _update(project_schema: ProjectCreate, current_user: User = Depends(get_current_user), session: Session = Depends(get_session)):
+    """
+    update project, and return schema of project
+    """
+    user_id = current_user.id
+    project_schema = ProjectCreatePri(user_id=user_id, **project_schema.dict(exclude_unset=True))
+    project_model = crud_project.update(session, obj_schema=project_schema)
+    return ProjectSchema(**project_model.to_dict())
