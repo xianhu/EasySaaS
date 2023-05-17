411729aa007c9cfbccc62162ddc148b6c496936f
xianhu <qixianhu@qq.com>
2023-05-17 12:03:42 +0800

    update files/upload

--- api/files.py
+++ api/files.py
@@ -4,21 +4,23 @@
 files api
 """
 
-from fastapi import APIRouter, Request, UploadFile
+from fastapi import APIRouter, Depends, UploadFile
 
 from core.settings import settings
+from data.models import User
 from data.schemas import Result
+from .utils import get_current_user
 
 # define router
 router = APIRouter()
 
 
 @router.post("/upload", response_model=Result)
-def _upload(file: UploadFile, request: Request):
+def _upload(file: UploadFile, current_user: User = Depends(get_current_user)):
     """
-    upload file
+    upload file: flowChunkNumber/flowTotalChunks/...for flow.js
     """
-    file_path = f"{settings.FOLDER_UPLOAD}/{file.filename}"
-    with open(file_path, "wb") as file_in:
+    file_name = f"{current_user.id}_{file.filename}"
+    with open(f"{settings.FOLDER_UPLOAD}/{file_name}", "wb") as file_in:
         file_in.write(file.file.read())
     return Result(msg="upload success")
