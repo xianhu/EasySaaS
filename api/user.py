f45a16ac43ca9496f862a3ae3106a5563f5113b6
xianhu <qixianhu@qq.com>
2023-05-17 21:41:43 +0800

    update user api

--- api/user.py
+++ api/user.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import Session
 from data import get_session
 from data.crud import crud_user
 from data.models import User
-from data.schemas import UserSchema, UserUpdate
+from data.schemas import UserSchema, UserUpdate, UserUpdatePri
 from .utils import get_current_user
 
 # define router
@@ -30,5 +30,6 @@ def _update(user_schema: UserUpdate, current_user: User = Depends(get_current_us
     """
     update schema of current_user
     """
+    user_schema = UserUpdatePri(**user_schema.dict(exclude_unset=True))
     current_user = crud_user.update(session, obj_model=current_user, obj_schema=user_schema)
     return UserSchema(**current_user.to_dict())
