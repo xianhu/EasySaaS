033523dcad2c203c1097d6d22720b9b613e9483f
xianhu <qixianhu@qq.com>
2023-05-23 15:56:22 +0800

    update

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ app = FastAPI(title=settings.APP_NAME, version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost",
-        "http://localhost:8000",
+        "http://127.0.0.1",
+        "http://127.0.0.1:8000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
