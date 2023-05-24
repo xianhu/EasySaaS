cb5c6516060d37cc35f9469a5a3c01a92f431552
xianhu <qixianhu@qq.com>
2023-05-24 22:23:08 +0800

    add test of cookie, header, request and response

--- api/__init__.py
+++ api/__init__.py
@@ -4,12 +4,15 @@
 api module
 """
 
-from fastapi import APIRouter
+from typing import Union
+
+from fastapi import APIRouter, Cookie, Header
+from fastapi import Request, Response
 
 from . import auth, files, project, user
 
 # define api_router
-api_router = APIRouter()
+api_router = APIRouter(prefix="")
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(user.router, prefix="/user", tags=["user"])
 api_router.include_router(project.router, prefix="/project", tags=["project"])
@@ -18,3 +21,22 @@ api_router.include_router(files.router, prefix="/files", tags=["files"])
 
 @api_router.get("/")
 async def root(): return {"message": "visit /docs for more information"}
+
+
+@api_router.get("/test")
+async def test(request: Request,
+               response: Response,
+               session: Union[str, None] = Cookie(default=None),
+               user_agent: Union[str, None] = Header(default=None)):
+    response.set_cookie(key="fake-session", value="1234567890")
+    response.headers["X-Fake-Header"] = "1234567890"
+    return {
+        "request": {
+            "cookies": request.cookies,
+            "headers": request.headers,
+            "client.host": request.client.host,
+            "client.port": request.client.port,
+        },
+        "session": session,
+        "user_agent": user_agent,
+    }
