0e411af5b03067016023e1f140262689006dcec4
xianhu <qixianhu@qq.com>
2023-06-05 19:59:37 +0800

    use debug in main

--- main.py
+++ main.py
@@ -21,7 +21,13 @@ log_format = "%(asctime)s %(levelname)s %(filename)s: %(message)s"
 logging.basicConfig(format=log_format, level=logging.WARNING, datefmt=None)
 
 # create app
-app = FastAPI(title=settings.APP_NAME, version="1.0.0")
+app = FastAPI(
+    debug=settings.DEBUG,
+    title=settings.APP_NAME,
+    version="1.0.0-beta" if settings.DEBUG else "1.0.0",
+    docs_url="/docs" if settings.DEBUG else None,
+    redoc_url="/redoc" if settings.DEBUG else None,
+)
 
 # set middleware
 app.add_middleware(
