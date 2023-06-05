bb3082ed19a7b5d0f629b64a732901c434624878
xianhu <xianhu@users.noreply.github.com>
2023-06-05 21:39:39 +0800

    N4.3.4 (#141) use debug in main, add version and use it in main, add xxx to readme.md
    * use debug in main
    
    * update readme
    
    * add version to settings
    
    * add app_version to settings

--- README.md
+++ README.md
@@ -16,7 +16,11 @@ docker inspect mysql/redis | grep IPAddress
 
 ```
 # vim .bash_profile / .zshrc
+export {PRE}_DEBUG=1
+
 export {PRE}_APP_NAME=EasySaaS
+export {PRE}_APP_VERSION=0.0.1
+
 export {PRE}_APP_DOMAIN=http://127.0.0.1:8000
 export {PRE}_SECRET_KEY=xxxxxxxxxxxxxxxxxxxxxxx
 
--- main.py
+++ main.py
@@ -21,7 +21,13 @@ log_format = "%(asctime)s %(levelname)s %(filename)s: %(message)s"
 logging.basicConfig(format=log_format, level=logging.WARNING, datefmt=None)
 
 # create app
-app = FastAPI(title=settings.APP_NAME, version="1.0.0")
+app = FastAPI(
+    debug=settings.DEBUG,
+    title=settings.APP_NAME,
+    version=settings.APP_VERSION,
+    docs_url="/docs" if settings.DEBUG else None,
+    redoc_url="/redoc" if settings.DEBUG else None,
+)
 
 # set middleware
 app.add_middleware(
@@ -74,12 +80,13 @@ async def shutdown_event():
 
 
 @app.middleware("http")
-async def add_process_time(request: Request, call_next):
+async def add_values_to_headers(request: Request, call_next):
     """
-    add process time to response header
+    add some values to response headers
     """
     start_time = time.time()
     response = await call_next(request)
-    process_time = time.time() - start_time
-    response.headers["X-Process-Time"] = str(process_time)
+    response.headers["X-Debug"] = str(settings.DEBUG)
+    response.headers["X-Version"] = str(settings.APP_VERSION)
+    response.headers["X-Duration"] = str(time.time() - start_time)
     return response
