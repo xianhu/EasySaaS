46543ac34acd3bd90594b33e8b1551937d402209
xianhu <qixianhu@qq.com>
2023-06-07 21:37:24 +0800

    update tasks accordings to debug

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -8,19 +8,15 @@ from celery import Celery
 
 from core.settings import settings
 
+# define broker and backend
+broker = f"{settings.REDIS_URI}/{11 if settings.DEBUG else 13}"
+backend = f"{settings.REDIS_URI}/{12 if settings.DEBUG else 14}"
+
 # celery -A tasks worker -l INFO --purge
 # celery -A tasks flower --port=5555 -l INFO
-app_celery = Celery(
-    __name__,
-    broker=f"{settings.REDIS_URI}/11",
-    backend=f"{settings.REDIS_URI}/12",
-    accept_content=["json", ],
-    task_serializer="json",
-    result_serializer="json",
-    include=[
-        "pages.panalysis.ptasks",
-    ],
-)
+app_celery = Celery(__name__, broker=broker, backend=backend, include=[
+    "pages.panalysis.ptasks",
+])
 
 
 @app_celery.task(name="tasks.add")
