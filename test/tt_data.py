4d76767a254b33cea7300b0d8028ebf5bac9e2db
xianhu <qixianhu@qq.com>
2023-07-02 20:09:49 +0800

    update

--- test/tt_data.py
+++ test/tt_data.py
@@ -22,21 +22,22 @@ with SessionMaker() as session:
     email = EmailStr("admin@easysaas.com")
     password = security.get_password_hash("a123456")
 
-    # create user -- schema of UserCreate
+    # create user -- schema of UserCreate, model of User
     user_schema = UserCreate(email=email, password=password)
-
-    # create user -- model of User
     user_model = User(id=100000, name=email, **user_schema.dict(exclude_unset=True))
     session.add(user_model)
     session.commit()
     logging.warning(user_model.to_dict())
+    logging.warning(UserSchema(**user_model.to_dict()).dict(exclude_unset=True))
 
-    # update user -- schema of UserUpdate
+    # update user -- schema of UserUpdate, model of User
     user_schema = UserUpdate(name="admin", avatar="http://www.easysaas.com")
     for field in user_schema.dict(exclude_unset=True):
         setattr(user_model, field, getattr(user_schema, field))
+    user_model.email_verified = True
     user_model.system_admin = True
-    user_model.system_role = {"role": "admin", "scopes": ["user:read", ]}
+    user_model.system_role = {"scopes": ["user:read", ]}
     session.merge(user_model)
     session.commit()
     logging.warning(user_model.to_dict())
+    logging.warning(UserSchema(**user_model.to_dict()).dict(exclude_unset=True))
