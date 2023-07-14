508050d5c576a8d31a5f95851b55c8b8520507fd
xianhu <xianhu@users.noreply.github.com>
2023-07-14 12:00:52 +0800

    M4.5.0 (#157) add audience, secret_key to jwt functions, add current_user to file api
    * update jwt: add audience and make params default
    
    * update
    
    * add current_user to file api

--- core/security.py
+++ core/security.py
@@ -13,37 +13,44 @@ from passlib.context import CryptContext
 
 from .settings import settings
 
-# global
-ALGORITHM = "HS256"
+# global variables
 pwd_context = CryptContext(schemes=["bcrypt", ], deprecated="auto")
 
 
-def create_token_data(data: Dict[str, Any], expires_duration: int = None) -> str:
+def create_jwt_token(subject: str,  # user_id, email, ...
+                     audience: str = None,  # client name or id, default None
+                     expire_duration: int = settings.ACCESS_TOKEN_EXPIRE_DURATION,
+                     secret_key: str = settings.SECRET_KEY,  # secret key
+                     algorithm: str = "HS256",  # algorithm of jwt
+                     **kwargs) -> str:  # other info to be stored in token
     """
-    create token based on data and expires_duration, return token
-    :param data: {"sub": user_id, "scopes": ["user:read", ...,], ...}
-    :param expires_duration: seconds, default ACCESS_TOKEN_EXPIRE_DURATION
+    create jwt token based on subject, audience and expire_duration
     """
-    # define expire value
-    seconds = expires_duration or settings.ACCESS_TOKEN_EXPIRE_DURATION
-    expire = datetime.utcnow() + timedelta(seconds=seconds)
+    # define expiration time
+    expiration_time = datetime.utcnow() + timedelta(seconds=expire_duration)
 
-    # define payload and token
-    payload = dict(**data, exp=expire)
-    token = jwt.encode(payload, settings.SECRET_KEY, algorithm=ALGORITHM)
+    # define payload - sub, exp, aud, nbf, ...
+    payload = dict(sub=subject, exp=expiration_time, **kwargs)
+    if audience:
+        payload["aud"] = audience
+    token = jwt.encode(payload, secret_key, algorithm=algorithm)
 
     # return
     return token
 
 
-def get_token_payload(token: str) -> Dict[str, Any]:
+def get_jwt_payload(token: str,  # token value
+                    audience: str = None,  # client name or id, default None
+                    secret_key: str = settings.SECRET_KEY,  # secret key
+                    algorithm: str = "HS256") -> Dict[str, Any]:
     """
     get payload from token, return payload or {} if failed
     """
     try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=ALGORITHM)
+        # decode token and get payload, raise error if audience not match
+        payload = jwt.decode(token, secret_key, algorithms=algorithm, audience=audience)
     except jwt.JWTError as excep:
-        logging.error("get token payload error: %s", excep)
+        logging.error("get jwt payload error: %s", excep)
         payload = {}
 
     # return
