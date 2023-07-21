803935634a58b821a79e2e46fa5fe452de2424c3
xianhu <qixianhu@qq.com>
2023-07-21 10:33:14 +0800

    send email of code

--- core/utemail.py
+++ core/utemail.py
@@ -19,23 +19,36 @@ smtp_options = {
     "password": settings.MAIL_PASSWORD,
     "tls": False, "ssl": True, "timeout": 10,
 }
+mail_from = (settings.APP_NAME, settings.MAIL_USERNAME)
 
 
-def send_email(_from: Union[str, tuple],
-               _to: Union[str, tuple],
-               subject: str,
-               html_raw: str,
-               render: Dict[str, Any]) -> int:
+def _send_email(mail_to: Union[str, tuple],
+                subject: str,
+                html_raw: str,
+                render: Dict[str, Any]) -> int:
     """
     send email via smtp, return status code
     """
     # define Jinja template
-    jj_html = JinjaTemplate(html_raw)
     jj_subject = JinjaTemplate(subject)
+    jj_html = JinjaTemplate(html_raw)
 
     # define message and send it
-    message = emails.Message(mail_from=_from, subject=jj_subject, html=jj_html)
-    response = message.send(to=_to, render=render, smtp=smtp_options)
+    message = emails.Message(mail_from=mail_from, subject=jj_subject, html=jj_html)
+    response = message.send(to=mail_to, render=render, smtp=smtp_options)
 
     # return status_code - 250
     return response.status_code
+
+
+def send_email_of_code(code: int, mail_to: Union[str, tuple]) -> int:
+    """
+    send email of code, return status code
+    """
+    # define email content and render
+    mail_subject = "Verify of {{ app_name }}"
+    mail_html = "Verify code: <b>{{ code }}</b>"
+    render = dict(app_name=settings.APP_NAME, code=code)
+
+    # send email and return
+    return _send_email(mail_to, subject=mail_subject, html_raw=mail_html, render=render)
