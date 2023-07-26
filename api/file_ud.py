9fdba2711f2f1953fdbc2e948fc2732cd46ad15e
xianhu <qixianhu@qq.com>
2023-07-26 10:57:34 +0800

    update file_ud api

--- api/file_ud.py
+++ api/file_ud.py
@@ -17,7 +17,7 @@ from sqlalchemy.orm import Session
 from core.settings import settings
 from core.utils import get_id_string, iter_file
 from data import get_session
-from data.models import File, FileTagFile, User
+from data.models import File, User
 from data.schemas import FileSchema, Resp
 from .utils import get_current_user
 
@@ -27,45 +27,43 @@ router = APIRouter()
 
 # response model
 class RespFile(Resp):
-    data: FileSchema = Field(None)
+    data_file: FileSchema = Field(None)
 
 
 @router.post("/upload", response_model=RespFile)
 def _upload(file: UploadFile = UploadFileClass(..., description="file object"),
-            filetag_id: str = Form(..., description="id of filetag"),
             current_user: User = Depends(get_current_user),
             session: Session = Depends(get_session)):
     """
-    upload file object and link to filetag, return file schema
+    upload file object, return file schema
     - **status_code=500**: file size too large
     """
+    user_id = current_user.id
+
     # check file size or raise exception
     if file.size > settings.MAX_FILE_SIZE:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="file size too large"
         )
-    filename = file.filename
+    filename, filesize = file.filename, file.size
+    file_kwargs = dict(filename=filename, filesize=filesize)
 
     # define fullname, location and save file
-    fullname = f"{current_user.id}-{int(time.time())}-{filename}"
+    fullname = f"{user_id}-{int(time.time())}-{filename}"
     location = f"{settings.FOLDER_UPLOAD}/{fullname}"
     with open(location, "wb") as file_in:
         file_in.write(file.file.read())
-    filesize = file.size
+    file_kwargs.update(dict(fullname=fullname, location=location))
     file_id = get_id_string(fullname)
-    filetagfile_id = get_id_string(f"{filetag_id}-{file_id}")
 
     # create file model and save to database
-    file_model = File(id=file_id,
-                      filename=filename, filesize=filesize,
-                      fullname=fullname, location=location)
-    filetagfile_model = FileTagFile(id=filetagfile_id, filetag_id=filetag_id, file_id=file_id)
-    session.add_all([file_model, filetagfile_model])
+    file_model = File(id=file_id, **file_kwargs)
+    session.add(file_model)
     session.commit()
 
-    # return file schema with permission
-    return RespFile(data=FileSchema(**file_model.dict()))
+    # return file schema
+    return RespFile(data_file=FileSchema(**file_model.dict()))
 
 
 @router.post("/upload-flow", response_model=RespFile)
@@ -74,13 +72,14 @@ def _upload_flow(file: UploadFile = UploadFileClass(..., description="part of fi
                  flow_chunk_total: int = Form(..., alias="flowChunkTotal"),
                  flow_total_size: int = Form(..., alias="flowTotalSize"),
                  flow_identifier: str = Form(..., alias="flowIdentifier"),
-                 filetag_id: str = Form(..., description="id of filetag"),
                  current_user: User = Depends(get_current_user),
                  session: Session = Depends(get_session)):
     """
     upload file object by flow.js, return file schema
     - **status_code=500**: file size too large
     """
+    user_id = current_user.id
+
     # check file size or raise exception
     if flow_total_size > settings.MAX_FILE_SIZE:
         raise HTTPException(
@@ -99,28 +98,25 @@ def _upload_flow(file: UploadFile = UploadFileClass(..., description="part of fi
     # check if all parts are uploaded
     if flow_chunk_number != flow_chunk_total:
         return RespFile(msg="uploading")
-    filename = file.filename
+    filename, filesize = file.filename, file.size
+    file_kwargs = dict(filename=filename, filesize=filesize)
 
     # define fullname, location and save file
-    fullname = f"{current_user.id}-{int(time.time())}-{filename}"
+    fullname = f"{user_id}-{int(time.time())}-{filename}"
     location = f"{settings.FOLDER_UPLOAD}/{fullname}"
     with open(location, "wb") as file_in:
         with open(location_temp, "rb") as file_temp:
             file_in.write(file_temp.read())
-    filesize = flow_total_size
+    file_kwargs.update(dict(fullname=fullname, location=location))
     file_id = get_id_string(fullname)
-    filetagfile_id = get_id_string(f"{filetag_id}-{file_id}")
 
     # create file model and save to database
-    file_model = File(id=file_id,
-                      filename=filename, filesize=filesize,
-                      fullname=fullname, location=location)
-    filetagfile_model = FileTagFile(id=filetagfile_id, filetag_id=filetag_id, file_id=file_id)
-    session.add_all([file_model, filetagfile_model])
+    file_model = File(id=file_id, **file_kwargs)
+    session.add(file_model)
     session.commit()
 
-    # return file schema with permission
-    return RespFile(data=FileSchema(**file_model.dict()))
+    # return file schema
+    return RespFile(data_file=FileSchema(**file_model.dict()))
 
 
 @router.get("/{file_id}", response_class=FileResponse)
@@ -131,10 +127,13 @@ def _download(file_id: str = Path(..., description="id of file"),
     download file by file_id, return FileResponse
     - **status_code=500**: file not existed
     """
-    # check if file existed
+    # check file_id and get location
     file_model = session.query(File).get(file_id)
-    if (not file_model) or (file_model.user_id != current_user.id):
-        return Resp(status=-1, msg="file not existed")
+    if not file_model:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="file not existed",
+        )
     location = file_model.location
 
     # check if file existed
@@ -143,7 +142,7 @@ def _download(file_id: str = Path(..., description="id of file"),
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="file not existed",
         )
-    filename = "-".join(file_id.split("-")[2:])
+    filename = file_model.filename
 
     # return file response
     return FileResponse(location, filename=filename)
@@ -157,10 +156,13 @@ def _download_stream(file_id: str = Path(..., description="id of file"),
     download file by file_id, return StreamingResponse
     - **status_code=500**: file not existed
     """
-    # check if file existed
+    # check file_id and get location
     file_model = session.query(File).get(file_id)
-    if (not file_model) or (file_model.user_id != current_user.id):
-        return Resp(status=-1, msg="file not existed")
+    if not file_model:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="file not existed",
+        )
     location = file_model.location
 
     # check if file existed
@@ -169,7 +171,7 @@ def _download_stream(file_id: str = Path(..., description="id of file"),
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="file not existed",
         )
-    filename = "-".join(file_id.split("-")[2:])
+    filename = file_model.filename
 
     # return streaming response
     headers = {"Content-Disposition": f"attachment; filename=\"{filename}\""}
