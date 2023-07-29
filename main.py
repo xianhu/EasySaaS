2b97eb3d61d5445c0e7c87623a01d4d1bc04fdde
xianhu <qixianhu@qq.com>
2023-07-29 11:29:57 +0800

    update docs

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ logging.basicConfig(format=log_format, level=logging.WARNING, datefmt=None)
 description = """
 - return 0 when success
 - return -1, -2, ... when something wrong in server
-- return HttpException(401) when access_token is invalid
+- return HttpException(401) when access_token is invalid or expired
+- return HttpException(403) when permission to access source is denied
 - return HttpException(500) when something wrong in file uploading
 - return HttpException(500) Internal Server Error when something wrong in server
 """
