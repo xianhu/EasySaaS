66739990ef4c939dfb5f14f707e6b44d46f56556
xianhu <qixianhu@qq.com>
2023-08-01 22:32:30 +0800

    update tasks

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -5,6 +5,7 @@ tasks module
 """
 
 import logging
+from typing import Optional
 
 from celery import Celery
 
@@ -16,13 +17,14 @@ backend = f"{settings.REDIS_URI}/{11 if not settings.DEBUG else 13}"
 
 # celery -A tasks flower -l INFO --port=5555
 # celery -A tasks worker -l INFO --concurrency=4 --purge
-app_celery = Celery(__name__, broker=broker, backend=backend, include=[
-    # "pages.panalysis.ptasks",
-])
+app_celery = Celery(__name__, broker=broker, backend=backend, include=[])
+
+# config of celery
+app_celery.conf.broker_connection_retry_on_startup = True
 
 
 @app_celery.task(bind=True, rate_limit="50/m")
-def test_add(self, x, y, z=None):
+def test_add(self, x: int, y: int, z: Optional[int] = None):
     # task information
     request = self.request
     logging.warning("%s: %s, %s", request.id, request.retries, request.delivery_info)
