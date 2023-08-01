60d09d7d091e5ce9b1c404e11c42d10d6573b6e1
xianhu <qixianhu@qq.com>
2023-08-01 10:28:02 +0800

    update

--- data/utils.py
+++ data/utils.py
@@ -34,7 +34,7 @@ def init_user_object(user_schema: UserCreate, session: Session) -> User:
     initialize user object based on create schema
     """
     try:
-        # create user model and add to database
+        # create user model based on create schema
         user_id = get_id_string(f"{user_schema.password}-{time.time()}")
         user_model = User(id=user_id, **user_schema.model_dump(exclude_unset=True))
         session.add(user_model)
