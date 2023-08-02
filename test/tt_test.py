841ca23f42961ee6851efe214a866474b39f9bff
xianhu <qixianhu@qq.com>
2023-08-02 16:02:14 +0800

    fix bugs in init_user

--- test/tt_test.py
+++ test/tt_test.py
@@ -8,7 +8,7 @@ import logging
 import time
 
 from core.security import create_jwt_token, get_jwt_payload
-from core.utemail import send_email_of_code
+from core.utemail import send_email_of_code, send_phone_of_code
 
 # test jwt and payload -- expire_duration
 token = create_jwt_token("111", expire_duration=10)
@@ -22,5 +22,6 @@ logging.warning("get payload: %s", get_jwt_payload(token))
 token = create_jwt_token("111", audience="web")
 logging.warning("get payload: %s", get_jwt_payload(token, audience="web"))
 
-# test send email
-send_email_of_code(123456, "qixianhu@qq.com")
+# test send email and phone
+logging.warning(send_email_of_code(123456, "qixianhu@qq.com"))
+logging.warning(send_phone_of_code(123456, "+86-18675768543"))
