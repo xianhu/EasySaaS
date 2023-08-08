c8ac14c0d1141b6efc9b9206b42253d800207cb9
xianhu <qixianhu@qq.com>
2023-08-08 21:41:13 +0800

    update delete process

--- api/utils.py
+++ api/utils.py
@@ -95,11 +95,17 @@ def delete_user_object(user_model: User, session: Session) -> bool:
     """
     user_id = user_model.id
     try:
+        # delete userproject models related to user
+        session.query(UserProject).filter(UserProject.user_id == user_id).delete()
+
         # delete filetag models related to user
         session.query(FileTag).filter(FileTag.user_id == user_id).delete()
 
         # delete file models related to user
         session.query(File).filter(File.user_id == user_id).delete()
+
+        # delete userlog models related to user
+        session.query(UserLog).filter(UserLog.user_id == user_id).delete()
 
         # delete user model
         session.delete(user_model)
