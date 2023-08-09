22b2ec589d8e55ebdef86d48fbc6486b704fc298
xianhu <qixianhu@qq.com>
2023-08-09 15:58:32 +0800

    update test

--- test/tt_data.py
+++ test/tt_data.py
@@ -10,12 +10,13 @@ from pprint import pformat
 from api.utils import create_user_object  # noqa: F401
 from core import get_password_hash
 from data import SessionMaker
-from data.models import FileTag
+from data.models import File, FileTag, FileTagFile
 from data.schemas import UserCreateEmail, UserCreatePhone
 from data.utils import init_db_tables  # noqa: F401
 
 # init db with all tables
 init_db_tables(model_list=None)
+init_db_tables(model_list=[FileTag, File, FileTagFile])
 
 # init user with session
 with SessionMaker() as session:
