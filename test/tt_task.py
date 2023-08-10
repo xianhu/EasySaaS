c11fb270c23d1d4fc53c451f89587ab50825164f
xianhu <qixianhu@qq.com>
2023-08-10 14:17:03 +0800

    update tasks

--- test/tt_task.py
+++ test/tt_task.py
@@ -7,15 +7,22 @@ test tasks
 import logging
 import time
 
-from tasks import test_add
+from tasks import test_add_1, test_add_2
+from celery.result import AsyncResult
 
 task_list = []
 for i in range(300):
-    task = test_add.apply_async((i, i), kwargs={"z": i}, countdown=1)
+    queue = "high_priority" if i % 2 == 0 else "low_priority"
+    if i % 2 == 0:
+        task = test_add_1.apply_async(args=(i, i), kwargs={"z": i})
+    else:
+        task = test_add_2.apply_async(args=(i, i), kwargs={"z": i})
+    # task = test_add.apply_async(args=(i, i), kwargs={"z": i}, queue=queue)
     task_list.append(task)
 
 while True:
     logging.warning("=======" * 10)
+
     for task in task_list:
         # task.ready(), task.get()
         if task.status == "FAILURE":
@@ -24,6 +31,6 @@ while True:
         if task.status == "SUCCESS":
             logging.warning("%s: %s, %s", task.id, task.status, task.result)
             continue
-        # PENDING, STARTED, RETRY, REVOKED
+        # PENDING, RECEIVED, STARTED, RETRY, REVOKED
         logging.warning("%s: %s", task.id, task.status)
     time.sleep(5)
