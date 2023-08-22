7a77e6ca2cf32255e8c0848bf8cbaef13a366da7
xianhu <qixianhu@qq.com>
2023-08-23 00:06:17 +0800

    delete filetag_id_list from RespFile

--- api/file/updown.py
+++ api/file/updown.py
@@ -27,28 +27,35 @@ def _upload(file: UploadFile = UploadFileClass(..., description="file object"),
     - **status=-2**: filename invalid: xxx.xxx
     - **status=-3**: file type not supported
     - **status=-4**: file size exceed limit
+    - **status_code=404**: filetag not found
     - **status_code=500**: create file or link to filetag error
     """
     user_id = current_user.id
+    filter0 = File.user_id == user_id
     session_id = str(int(datetime.utcnow().timestamp() * 1000))
+
+    # check filetag_id_list
+    for filetag_id in filetag_id_list:
+        check_filetag(filetag_id, user_id, session)
 
     # check if file model existed based on session_id
     file_id = get_id_string(f"{user_id}-{session_id}")
     file_model = session.query(File).get(file_id)
     if file_model and file_model.status == 1:
+        # create file schema
         file_schema = FileSchema(**file_model.dict())
-        filetag_id_list = get_filetag_id_list(file_id, session)
-        return RespFile(status=1, msg="file existed in database",
-                        data_file=file_schema, data_filetag_id_list=filetag_id_list)
+        file_schema.filetag_id_list = get_filetag_id_list(file_id, session)
+
+        # return file schema
+        return RespFile(status=1, msg="file existed in database", data_file=file_schema)
 
     # check if file count exceed limit
-    filter0 = File.user_id == user_id
     if session.query(File).filter(filter0).count() >= FILE_LIMIT_COUNT:
         return RespFile(status=-1, msg="file count exceed limit")
 
-    # check if filename valid
-    filename = filename or file.filename
-    if (not filename) or (filename.find(".") < 0):
+    # check if filename invalid
+    filename = filename or file.filename or ""
+    if (len(filename) < 4) or (filename.find(".") < 0):
         return RespFile(status=-2, msg="filename invalid: xxx.xxx")
 
     # check if file type not supported
@@ -75,14 +82,15 @@ def _upload(file: UploadFile = UploadFileClass(..., description="file object"),
     try:
         # create file model based on filename, keywords, ...
         file_schema = FileCreate(filename=filename, keywords=keywords)
-        file_model = File(id=file_id, user_id=user_id, **file_schema.model_dump(exclude_unset=True),
-                          filesize=filesize, filetype=filetype, fullname=fullname, location=location)
+        file_model = File(id=file_id, user_id=user_id,
+                          **file_schema.model_dump(exclude_unset=True),
+                          filesize=filesize, filetype=filetype,
+                          fullname=fullname, location=location,
+                          edit_time=datetime.utcnow())
         session.add(file_model)
 
-        # link to filetag based on filetags
+        # link to filetag based on filetag_id_list
         for filetag_id in filetag_id_list:
-            check_filetag(filetag_id, user_id, session)
-
             # create filetagfile model by file_id and filetag_id
             filetagfile_model = FileTagFile(file_id=file_id, filetag_id=filetag_id)
             session.add(filetagfile_model)
@@ -91,16 +99,18 @@ def _upload(file: UploadFile = UploadFileClass(..., description="file object"),
         session.commit()
     except Exception as excep:
         session.rollback()
-        logging.error("create file error: %s", excep)
+        logging.error("create file or link to filetag error: %s", excep)
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="create file or link to filetag error",
         )
 
-    # return file schema and filetag_id list
+    # create file schema
     file_schema = FileSchema(**file_model.dict())
-    filetag_id_list = get_filetag_id_list(file_id, session)
-    return RespFile(data_file=file_schema, data_filetag_id_list=filetag_id_list)
+    file_schema.filetag_id_list = get_filetag_id_list(file_id, session)
+
+    # return file schema
+    return RespFile(data_file=file_schema)
 
 
 @router.get("/download/{file_id}", response_class=StreamingResponse)
@@ -113,9 +123,13 @@ def _download(file_id: str = Path(..., description="file id"),
     """
     # check file_id and get file model
     file_model = check_file(file_id, current_user.id, session)
-    filename, location = file_model.filename, file_model.location
-    filename_encoded = urllib_parse.quote(filename)
+
+    # define filename_encoded from filename
+    filename_encoded = urllib_parse.quote(file_model.filename)
+
+    # define file_stream from location
+    file_stream = iter_file(file_model.location)
 
     # return streaming response with filename in headers
     headers = {"Content-Disposition": f"attachment; filename*=UTF-8''{filename_encoded}"}
-    return StreamingResponse(iter_file(location), media_type="application/octet-stream", headers=headers)
+    return StreamingResponse(file_stream, media_type="application/octet-stream", headers=headers)
