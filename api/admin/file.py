2879cfe325b972446b1f4c2388d01c27ef173e79
xianhu <qixianhu@qq.com>
2023-08-23 14:14:32 +0800

    update admin

--- api/admin/file.py
+++ api/admin/file.py
@@ -1,11 +1,11 @@
 # _*_ coding: utf-8 _*_
 
 """
-file api of admin
+admin api of file
 """
 
 from ..base import *
-from ..file.utils import RespFileList
+from ..file.utils import RespFileList, get_filetag_id_list
 
 # define router
 router = APIRouter()
@@ -18,11 +18,14 @@ def _get_file_schema_list(skip: int = Query(0, description="skip count"),
     """
     get file schema list, support pagination
     """
-    # get file model list and schema list
-    file_model_list = (session.query(File)
-                       .order_by(File.created_at.desc())
-                       .offset(skip).limit(limit).all())
-    file_schema_list = [FileSchema(**fm.dict()) for fm in file_model_list]
+    # get file schema list
+    file_schema_list = []
+    for file_model in (session.query(File)
+            .order_by(File.created_at.desc())
+            .offset(skip).limit(limit).all()):
+        file_schema = FileSchema(**file_model.dict())
+        file_schema.filetag_id_list = get_filetag_id_list(file_model.id, session)
+        file_schema_list.append(file_schema)
 
     # return total count and file schema list
     file_total = session.query(File).count()
--- api/admin/user.py
+++ api/admin/user.py
@@ -1,7 +1,7 @@
 # _*_ coding: utf-8 _*_
 
 """
-user api of admin
+admin api of user
 """
 
 from ..base import *
